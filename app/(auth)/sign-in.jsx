4ed388fd988b5839ba3b035456d1f351c598a85b
B-Phan <brandon.phan2@ucalgary.ca>
2024-07-08 00:19:08 -0600

    sign-in screen configured

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,12 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../../constants';
+import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
 
 const SignIn = () => {
-  return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  });
 
-export default SignIn
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    } 
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-1 my-1">
+          <Image 
+            source={images.logo}
+            resizeMode='contain' 
+            className="w-[115px] h-[115px]"
+          />
+          <Text className="text-2xl text-white text-semibold mt-1 font-psemibold">
+            Log in
+          </Text>
+          <FormField 
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField 
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({...form, password: e})}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
 import CustomButton from '../components/CustomButton';
@@ -10,17 +10,19 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full items-center pt-6">
+        <View className="w-full justify-center items-center min-h h-[85vh] px-4">
           <Image 
             source={images.logo}
             className="w-40 h-40"
             resizeMode='contain'
           />
+
           <Image 
             source={images.cards}
-            className="max-w--[380px] w-full h-[300px]"
+            className="max-w-[380px] w-full h-[300px]"
             resizeMode='contain'
           />
+
           <View className='relative mt-5'>
             <Text className="text-3xl text-white 
             font-bold text-center">
@@ -29,6 +31,9 @@ export default function App() {
                 AIRith
               </Text>
             </Text>
+
+            </View>
+
             <Text className="text-sm font-pregular
              text-gray-100 mt-7 text-center">
               Innovation and Exploration at your fingertips
@@ -36,13 +41,14 @@ export default function App() {
 
             <CustomButton 
               title="Continue with Email"
-              handlePress={() => {}}
-              containerStyle="w-full mt-7"
+              handlePress={() => router.push('/sign-in')}
+              containerStyles="w-full mt-7"
             />
           </View>
-        </View>
       </ScrollView>
-      <StatusBar backgroundColor='#161622' style='light' />
+      
+      <StatusBar backgroundColor='#161622' 
+      style='light' />
     </SafeAreaView>
   );
 }
